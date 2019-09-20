ee413571a7fa73236e9ab6246bcaafd5372397a7
krishnagopi242 <krishnagopi242@gmail.com>
2019-09-20 22:49:12 +0530

    Login page updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,19 @@ const routes: Routes = [
     path: 'products',
     loadChildren: () => import('./layout/layout.module').then(layout => layout.LayoutModule)
   },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
 ];
 
 @NgModule({
