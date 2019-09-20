8f46a8d21a3f5accda59a3f9877b2b91a02a51d7
krishnagopi242 <krishnagopi242@gmail.com>
2019-09-20 20:11:30 +0530

    Removed unwanted code

--- src/app/layout/layout.routing.module.ts
+++ src/app/layout/layout.routing.module.ts
@@ -1,16 +1,3 @@
-// {
-//   path: 'product/create',
-//   component: ProductAddComponent
-// },
-// {
-//   path: 'edit/:id',
-//   component: ProductEditComponent
-// },
-// {
-//   path: 'products',
-//   component: ProductGetComponent
-// },
-
 import { Routes } from '@angular/router';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
